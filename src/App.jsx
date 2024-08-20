95046b4c0f1e93966eca81f9789068e3159f094d
Deepank kartikey <deepankkartikey@gmail.com>
2024-08-20 19:00:02 -0400

    Used Link to prevent page reloading and Add a JobsPage route

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,13 @@ import {
 import HomePage from './pages/HomePage';
 
 import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={ <MainLayout /> }>
     <Route index element= {<HomePage />}/>
+    <Route path='/jobs' element= {<JobsPage />}/>
   </Route>
   )
 );
