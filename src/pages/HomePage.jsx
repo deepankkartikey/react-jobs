7266b3d0415be8927f7904851cfffa089a86fbbb
Deepank kartikey <deepankkartikey@gmail.com>
2024-08-20 18:44:29 -0400

    Created a layout and HomePage route

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Hero from '../components/Hero';
+import HomeCards from '../components/HomeCards';
+import JobListings from '../components/JobListings';
+import ViewAllJobs from '../components/ViewAllJobs';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+
+    </>
   )
 }
 
