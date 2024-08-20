1d218c5d878a33ba317e3170e92da2caa8ba42b7
Deepank kartikey <deepankkartikey@gmail.com>
2024-08-20 19:41:08 -0400

    Get JobsData using useEffect hook from Server using API call

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-import jobs from '../jobs.json';
 import JobListing from './JobListing';
+
+import { useState, useEffect } from 'react';
 
 const JobListings = ({ isHome = false }) => {
 
-    const jobListings = isHome ? jobs.slice(0,3): jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try{
+        const res = await fetch('http://localhost:8000/jobs')
+        const jobData = await res.json();
+        setJobs(jobData);
+      }
+      catch(error){
+        console.log('Error fetching data', error);
+      }
+      finally {
+        setLoading(false);
+      }
+
+    }
+
+    fetchJobs();
+
+  },[])
 
   return (
     <>
@@ -14,7 +36,7 @@ const JobListings = ({ isHome = false }) => {
           { isHome ? 'Recent Jobs' : 'Browse Jobs' }
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            { jobListings.map((job) => (
+            { jobs.map((job) => (
                 <JobListing key = { job.id } job = { job } />
             ))}
         </div>
