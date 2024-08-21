1e3cd6001ba0be1dd5135ccd3148b1207facd0ef
Deepank kartikey <deepankkartikey@gmail.com>
2024-08-20 21:50:35 -0400

    Navigate to jobs page after submitting add job form

--- src/App.jsx
+++ src/App.jsx
@@ -16,20 +16,22 @@ import NotFound from './pages/NotFound';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={ <MainLayout /> }>
-    <Route index element= {<HomePage />}/>
-    <Route path='/jobs' element= {<JobsPage />}/>
-    <Route path='/add-job' element= {<AddJobPage />}/>
-    <Route path='/jobs/:id' element= {<JobPage />} loader={jobLoader}/>
-    <Route path='/*' element= {<NotFound />}/>
-  </Route>
-  )
-);
-
 const App = () => {
-  return <RouterProvider router={ router } />;
+    const addJob = (newJob) => {
+      console.log(newJob);
+    };
+
+    const router = createBrowserRouter(
+      createRoutesFromElements(
+        <Route path='/' element={ <MainLayout /> }>
+        <Route index element= {<HomePage />}/>
+        <Route path='/jobs' element= {<JobsPage />}/>
+        <Route path='/add-job' element= {<AddJobPage addJobSubmit={ addJob }/>}/>
+        <Route path='/jobs/:id' element= {<JobPage />} loader={jobLoader}/>
+        <Route path='/*' element= {<NotFound />}/></Route>
+      )
+    );
+   return <RouterProvider router={ router } />;
 }
 
 export default App
