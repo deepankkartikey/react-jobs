ca9517a0f493366db8cada1d4998911cb5509955
Deepank kartikey <deepankkartikey@gmail.com>
2024-08-20 22:39:47 -0400

    Completed Edit Job Posting functionality using DataLoader

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import JobsPage from './pages/JobsPage';
 import NotFound from './pages/NotFound';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
     // Add New Job
@@ -38,6 +39,18 @@ const App = () => {
       return;
     }
 
+    // Update a Job
+    const updateJob = async (updatedJobDetails) => {
+      const res = await fetch(`/api/jobs/${updatedJobDetails.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedJobDetails)
+      })
+      return;
+    };
+
     const router = createBrowserRouter(
       createRoutesFromElements(
         <Route path='/' element={ <MainLayout /> }>
@@ -45,6 +58,7 @@ const App = () => {
         <Route path='/jobs' element= {<JobsPage />}/>
         <Route path='/add-job' element= {<AddJobPage addJobSubmit={ addJob }/>}/>
         <Route path='/jobs/:id' element= {<JobPage deleteJob={ deleteJob }/>} loader={jobLoader}/>
+        <Route path='/edit-job/:id' element= {<EditJobPage updateJob = { updateJob }/>} loader={jobLoader}/>
         <Route path='/*' element= {<NotFound />}/></Route>
       )
     );
